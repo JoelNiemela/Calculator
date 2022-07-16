d127aa53f4c0d615689f049a9d04817220439eca
JoelNiemela <joel@niemela.se>
2022-07-16 17:21:02 -0500

    Implement display superscripts

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+let calcValue = [];
+let caretPos = [0];
+
+function calcValueString(value, pos=[]) {
+    const equals = (a, b) =>
+      a.length === b.length &&
+      a.every((v, i) => v === b[i]);
+
+  if (typeof value == "string") {
+    pos[pos.length-1]++;
+
+    if (equals(pos, caretPos)) return '<span class="caret">' + value + "</span>";
+    return value;
+  }
+
+  if (Array.isArray(value)) {
+    return value.map((e, i) => calcValueString(e, [...pos, i])).join("");
+  }
+
+  return "<sup>" + calcValueString(value.value, pos) + "</sup>";
+}
+
 function getElementOffset(element) {
     const de = document.documentElement;
     const box = element.getBoundingClientRect();
@@ -21,24 +43,63 @@ function riple(e) {
   setTimeout(function() {
     riple.remove();
   }, 2000);
-};
+}
+
+function moveCaret() {
+  caretPos[caretPos.length-1]++;
+}
+
+function insert(value, arr=calcValue, pos=caretPos) {
+  const [index, ...subIndex] = pos;
+
+  if (subIndex.length > 0) {
+    insert(value, arr[index].value, subIndex);
+  } else {
+    arr.splice(index, 0, value);
+  }
+}
+
+function remove(arr=calcValue, pos=caretPos) {
+  const [index, ...subIndex] = pos;
+
+  if (subIndex.length > 0) {
+    if (subIndex == 0 && arr[index].value.length == 0) {
+      arr.splice(index, 1);
+      caretPos.pop();
+    } else {
+      remove(arr[index].value, subIndex);
+    }
+  } else {
+    if (index > 0) {
+      arr.splice(index-1, 1);
+      caretPos[caretPos.length-1]--;
+    }
+  }
+}
 
 function handleInput(key) {
   let display = document.getElementById("display");
 
   switch (key) {
     case 'AC':
-      display.textContent = "";
+      calcValue = [];
       break;
     case '⌫':
-      display.textContent = display.textContent.slice(0, -1);
+      remove();
+      break;
+    case '^':
+      insert({type: "super", value: []});
+      caretPos.push(0);
       break;
     case '=':
       break;
     default:
-      display.textContent += key;
+      insert(key);
+      moveCaret();
       break;
   }
+
+  display.innerHTML = calcValueString(calcValue);
 }
 
 function handleButtonClick(e) {
