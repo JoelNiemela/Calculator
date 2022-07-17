c34946f96a9e2e8c8d382de8f7197aa4e75f94aa
JoelNiemela <joel@niemela.se>
2022-07-16 22:59:25 -0500

    Implement recursive decent parser

--- script.js
+++ script.js
@@ -93,6 +93,116 @@ function riple(e) {
   }, 2000);
 }
 
+function tokenize(str) {
+  const rules = [
+    ["num", /^[0-9]*\.?[0-9]+/],
+    ["var", /^[a-bA-BΑ-Ωα-ω]+/],
+    ["lpar", /^\(/],
+    ["rpar", /^\)/],
+    ["exp", /^\^/],
+    ["mul", /^\×/],
+    ["div", /^\÷/],
+    ["add", /^\+/],
+    ["sub", /^\-/],
+    ["err", /^\./],
+  ];
+
+  let pos = 0;
+  let tokens = [];
+
+  let token;
+  while (pos < str.length) {
+    for (const [type, regex] of rules) {
+      if (token = str.slice(pos).match(regex)) {
+        pos += token[0].length;
+        tokens.push({type, value: token[0]});
+      }
+    }
+  }
+
+  return tokens;
+}
+
+function parse(tokens, prec=10) {
+  if (prec == 0) {
+    const token = tokens.shift();
+    if (token?.type == "num") {
+      return { type: "num", value: parseFloat(token.value) };
+    } else if (token?.type == "var") {
+      return { type: "var", symbol: token.value };
+    } else if (token?.type == "lpar") {
+      const exp = parse(tokens);
+      const closePar = tokens.shift();
+      if (closePar?.type == "rpar") {
+        return { type: "par", exp: exp };
+      } else {
+        return { type: "par", exp: exp, err: "Unclosed parenthesis" };
+      }
+    }
+
+    return { type: "null", value: null };
+  }
+
+  const ops = {
+    '1': [],
+    '2': [],
+    '3': [],
+    '4': ['exp'],
+    '5': ['mul', 'div'],
+    '6': ['add', 'sub'],
+    '7': [],
+    '8': [],
+    '9': [],
+    '10': [],
+  };
+
+  const lexp = parse(tokens, prec-1);
+
+  const token = tokens.shift();
+  if (ops[prec].includes(token?.type)) {
+    const rexp = parse(tokens, prec-1);
+    return { type: token.type, lexp, rexp };
+  } else {
+    tokens.unshift(token);
+    return lexp;
+  }
+}
+
+function evaluate(exp, symtable) {
+  switch (exp?.type) {
+    case "num":
+      return exp.value;
+    case "var":
+      return symtable[exp.symbol];
+    case "null":
+      return evaluate(exp.value, symtable);
+    case "par":
+      return evaluate(exp.exp, symtable);
+    case "exp":
+      return evaluate(exp.lexp, symtable) ** evaluate(exp.rexp, symtable);
+    case "mul":
+      return evaluate(exp.lexp, symtable) * evaluate(exp.rexp, symtable);
+    case "div":
+      return evaluate(exp.lexp, symtable) / evaluate(exp.rexp, symtable);
+    case "add":
+      return evaluate(exp.lexp, symtable) + evaluate(exp.rexp, symtable);
+    case "sub":
+      return evaluate(exp.lexp, symtable) - evaluate(exp.rexp, symtable);
+  }
+}
+
+function calculate() {
+  const str = calcValueEquation(calcValue);
+  const tokens = tokenize(str);
+  const tree = parse(tokens);
+  const symtable = {
+    "e": 2.71828182846,
+    "π": 3.14159265359,
+  };
+  const value = evaluate(tree, symtable);
+  return value;
+}
+
 function getAt(pos, value=calcValue) {
   if (typeof value == "string") {
     return value;
@@ -186,7 +296,6 @@ function update() {
 }
 
 function handleInput(key) {
-
   switch (key) {
     case 'AC':
       calcValue = [];
@@ -210,6 +319,8 @@ function handleInput(key) {
     case '↓':
       break;
     case '=':
+      calcValue = calculate().toString().split("");
+      caretPos = [calcValue.length];
       break;
     default:
       insert(key);
